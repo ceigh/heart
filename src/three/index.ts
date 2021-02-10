f44d4505507b701f15bfc8b411cb5f5934f87700
Artjom Löbsack <me@ceigh.com>
2021-02-10 22:42:42 +0500

    feat: add stats on development

--- src/three/index.ts
+++ src/three/index.ts
@@ -1,3 +1,4 @@
+import Stats from 'stats.js'
 import { getCamera } from './camera'
 import { getScene } from './scene'
 import { getRenderer } from './renderer'
@@ -11,10 +12,19 @@ const renderer = getRenderer(w, h)
 const light = getLight()
 const heart = getHeart()
 
+const showStats = process.env.NODE_ENV === 'development'
+const stats = new Stats()
+if (showStats) {
+  stats.showPanel(0)
+  document.body.appendChild(stats.dom)
+}
+
 function animation (time: number) {
+  if (showStats) stats.begin()
   const heartScale = 0.015 + Math.abs(Math.sin(time / 1000)) / 300
   heart.scale.set(heartScale, heartScale, heartScale)
   renderer.render(scene, camera)
+  if (showStats) stats.end()
 }
 
 export function init (rendererContainer: HTMLElement) {

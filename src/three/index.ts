530d0b224461c1d5ac67c9b8976ef83ed0101dff
Artjom Löbsack <me@ceigh.com>
2021-02-12 01:50:46 +0500

    feat: add orbit controls

--- src/three/index.ts
+++ src/three/index.ts
@@ -1,17 +1,24 @@
 import Stats from 'stats.js'
 import { AxesHelper, CameraHelper } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { getCamera } from './camera'
 import { getScene } from './scene'
 import { getRenderer } from './renderer'
 import { getLights } from './light'
 import { getHeart, getFloor } from './mesh'
+import { degree } from './_'
 
 const [w, h] = [window.innerWidth, window.innerHeight]
 const camera = getCamera(w, h)
 const scene = getScene()
 const renderer = getRenderer(w, h)
+const controls = new OrbitControls(camera, renderer.domElement)
 const lights = getLights()
 const heart = getHeart()
+
+controls.minDistance = 5
+controls.maxDistance = 20
+controls.maxPolarAngle = 89 * degree
 
 lights.forEach(l => scene.add(l))
 scene.add(getFloor())
--- src/three/scene.ts
+++ src/three/scene.ts
@@ -1,6 +1,8 @@
-import { Scene } from 'three'
+import { Scene, Fog } from 'three'
 
 export function getScene (): Scene {
   const scene = new Scene()
+  const fog = new Fog(0x000000, 1, 40)
+  scene.fog = fog
   return scene
 }
